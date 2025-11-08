dbbff1892a201c283e8d3df0ef51cef9124d6dde
ksjsjsjs537 <lolaaaicaaa@gmail.com>
2025-11-08 20:01:42 +0700

    Add files via upload

---
+++ instagram.js
@@ -0,0 +1,20 @@
+const instagramGetUrl = require("imon-videos-downloader")
+
+async function igdl (url) {
+let links = await instagramGetUrl.instagram(url).then((res) => {
+return {
+status: true, 
+creator: "Ikyxx",
+result: res.data
+}
+}).catch((e) => {
+return {
+status: false, 
+creator: global.creator, 
+result: {}
+}
+})
+return links
+}
+
+module.exports = { igdl }
